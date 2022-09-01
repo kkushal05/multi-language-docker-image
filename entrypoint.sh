1e83a7b0517280de360420e868746715145763f7
Kushal Jain <kkushal05@yahoo.com>
2022-09-02 00:26:34 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-set -x
+#set -x
 source /root/.gvm/scripts/gvm
+bash << (sdkman install java ${JDK_VERSION_VAL})
+bash << (sdkman install cfx ${CFX_VERSION_VAL})
+bash << (nvm install -s  ${NODE_VERSION_VAL})
+bash << (gvm install go${GO_VERSION_VAL})
+
