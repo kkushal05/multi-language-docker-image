8145103eee3414fabba6ea5343f724bb50ea451c
Kushal Jain <kkushal05@yahoo.com>
2022-09-02 01:38:05 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
 set -x
-bash < < (source /root/.gvm/scripts/gvm)
-echo "JDK_VERSION_VAL : $JDK_VERSION_VAL"
-echo "CFX_VERSION_VAL : $CFX_VERSION_VAL"
-echo "NODE_VERSION_VAL : $NODE_VERSION_VAL"
-echo "GO_VERSION_VAL : $GO_VERSION_VAL"
-bash < < (sdkman install java ${JDK_VERSION_VAL})
-bash < < (sdkman install cfx ${CFX_VERSION_VAL})
-bash < < (nvm install -s  ${NODE_VERSION_VAL})
-bash < < (gvm install go${GO_VERSION_VAL})
 
+source /root/.gvm/scripts/gvm
+source $HOME/.sdkman/bin/sdkman-init.sh
+
+if [ -z "$JDK_VERSION_VAL" ];
+then
+    echo "Skipping installation. JDK_VERSION_VAL is empty"
+else
+    echo "Installing Java ${JDK_VERSION_VAL}"
+    sdk install java ${JDK_VERSION_VAL}
+fi
+
+if [ -z "$CFX_VERSION_VAL" ];
+then
+    echo "Skipping installation. CFX_VERSION_VAL is empty"
+else
+    echo "Installing CFX ${CFX_VERSION_VAL}"
+    sdk install cfx ${CFX_VERSION_VAL}
+fi
+
+if [ -z "$NODE_VERSION_VAL" ];
+then
+    echo "Skipping installation. NODE_VERSION_VAL is empty"
+else
+    echo "Installing Node ${NODE_VERSION_VAL}"
+    nvm install -s ${NODE_VERSION_VAL}
+fi
+
+if [ -z "$GO_VERSION_VAL" ];
+then
+    echo "Skipping installation. GO_VERSION_VAL is empty"
+else
+    echo "Installing Go ${GO_VERSION_VAL}"
+    gvm install  go${GO_VERSION_VAL}
+    gvm use   go${GO_VERSION_VAL}
+fi
