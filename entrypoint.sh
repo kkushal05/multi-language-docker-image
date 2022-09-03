a13d1ad56e275fd5771f05c93b25a1b4f2224ea0
Kushal Jain <kkushal05@yahoo.com>
2022-09-03 21:52:50 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,13 @@ set -x
 source /root/.gvm/scripts/gvm
 source $HOME/.sdkman/bin/sdkman-init.sh
 
+
+#INSTALLING TERRAFORM
+release=`curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest |  grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'`
+wget https://releases.hashicorp.com/terraform/${release}/terraform_${release}_linux_amd64.zip
+unzip terraform_${release}_linux_amd64.zip
+mv terraform /usr/bin/terraform
+
 if [ -z "$JDK_VERSION_VAL" ];
 then
     echo "Skipping installation. JDK_VERSION_VAL is empty"
